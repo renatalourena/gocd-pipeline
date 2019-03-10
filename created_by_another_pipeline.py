4d1476c25afde31b1b3dbdab1fc9778866545502
Renata Lourena <renata.lourena@enernoc.com>
2019-03-10 13:24:16 -0300

    Add script to pipeline created by another

---
+++ created_by_another_pipeline.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from gomatic import *
+
+configurator = GoCdConfigurator(HostRestClient("localhost:8153", ssl=False))
+pipeline = configurator\
+	.ensure_pipeline_group("createdGroup")\
+	.ensure_replacement_of_pipeline("CreatedByAnotherPipeline")\
+	.set_git_url("https://github.com/renatalourena/dojo_scala_fisl_17.git")
+stage = pipeline.ensure_stage("createdStage")
+job = stage.ensure_job("createdJob")
+job.add_task(ExecTask(['pwd']))
+
+configurator.save_updated_config(save_config_locally=True, dry_run=True)
