c0acaf22de4137332e26ccb24d3a9882200fc146
Renata Lourena <renata.lourena@enernoc.com>
2019-03-10 14:29:28 -0300

    Remove dry run configuration

--- created_by_another_pipeline.py
+++ created_by_another_pipeline.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from gomatic import *
 
-configurator = GoCdConfigurator(HostRestClient("172.17.0.2:8153", ssl=False))
+configurator = GoCdConfigurator(HostRestClient("localhost:8153", ssl=False))
 pipeline = configurator\
 	.ensure_pipeline_group("createdGroup")\
 	.ensure_replacement_of_pipeline("CreatedByAnotherPipeline")\
@@ -10,4 +10,4 @@ stage = pipeline.ensure_stage("createdStage")
 job = stage.ensure_job("createdJob")
 job.add_task(ExecTask(['pwd']))
 
-configurator.save_updated_config(save_config_locally=True, dry_run=True)
+configurator.save_updated_config()
